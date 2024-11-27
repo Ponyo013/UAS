b9e8213d6e2aeeee468e555e3962e5a7a3bc779a
Cinamon <huangfredlie04@gmail.com>
2024-11-27 17:31:21 +0700

    smooth scroll

--- UAS-LEC/resources/views/layouts/app.blade.php
+++ UAS-LEC/resources/views/layouts/app.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title', 'Tunas Mahardika')</title>
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased light:bg-light text-white" style="background-color: #FFFF;">
-        @include('part.navbar')
-        <main>
-            @yield('content')
-        </main>
-        @include('part.footer')
-    </body>
+<html class="scroll-smooth" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Tunas Mahardika')</title>
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased light:bg-light text-white" style="background-color: #FFFF;">
+    @include('part.navbar')
+    <main>
+        @yield('content')
+    </main>
+    @include('part.footer')
+</body>
+
 </html>
